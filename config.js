02f94f51fa547d8ac2f4a9d3a9b55461d713d977
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-09-19 19:43:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,9 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   OWNER_NAME: process.env.OWNER_NAME || "ENTER YOUR NAME",
-  PRIFIX:process.env.PRIFIX || ".,/<>"
+  PRIFIX:process.env.PRIFIX || ".,/<>",
+  ALIVE_IMG:process.env.AlIVE_IMG || "https://files.catbox.moe/80oe96.jpg",
+  ALIVE_MSG:process.env.ALIVE_MSG || "I am Alive Now!! 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝",
+  AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
+  MODE:process.env.MODE || "public",//you can change it to (public,privet,group,inbox)
 };
