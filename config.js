8c66de91cf56cf2617512b31d7750a532054ded9
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-09-19 19:14:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   OWNER_NAME: process.env.OWNER_NAME || "ENTER YOUR NAME",
 };
