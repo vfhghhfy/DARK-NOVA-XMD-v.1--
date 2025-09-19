79cd7afe63376efad6039f0a18458e0711293a74
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-09-19 20:04:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const { sms, downloadMediaMessage } = require("./lib/msg");
 const axios = require("axios");
 const { File } = require("megajs");
 const prefix = config.PREFIX;
+const {default: fetch} = import('node-fetch');
 
 const ownerNumber = config.OWNER_NUM;
 
