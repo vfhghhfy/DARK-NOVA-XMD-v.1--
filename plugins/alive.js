13adca22a046ec5a318df76475ee90e3fa833099
alpha-x-team-ofc <alphaxteam21@gmail.com>
2025-09-19 19:19:32 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,5 @@
-const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
+const config = requrie('../config'); 
 
 cmd({
     pattern: "alive",
@@ -9,7 +9,6 @@ cmd({
 },
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
